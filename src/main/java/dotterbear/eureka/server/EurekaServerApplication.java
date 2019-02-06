fc1f078117725746a33484d8dfc7de1b6ccfd806
pqab.annie <pqab.annie@gmail.com>
2019-02-06 21:01:25 +0800

    update package

--- src/main/java/dotterbear/eureka/server/EurekaServerApplication.java
+++ src/main/java/dotterbear/eureka/server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package src.main.java.dotterbear.eureka.server;
+package dotterbear.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
